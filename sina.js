bd3369f15f3cbbeda7621bcec53bbe0e5b8e8896
Sina <simacna@gmail.com>
2014-11-25 13:24:42 -0500

    fixed href issues for menu options

--- sina.js
+++ sina.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-	$('ul li.buttons').hover(function(){
-		$(this).children('ul').fadeIn(300, 1);
-	}, function(){
-		$(this).children('ul').fadeOut(300, 0);
-	})
+/*$(document).ready(function(){
+$('ul li.buttons').hover(function(){
+$(this).children('ul').fadeIn(300, 1);
+}, function(){
+$(this).children('ul').fadeOut(300, 0);
 })
+})
+*/
+
+
+$(document).ready(function () {
+	$(".menu ul li").hover(function(){
+		$(this).children("ul").stop().slideDown(300);
+	},
+	function(){
+		$(this).children("ul").stop().slideUp(300);
+	});
+
+	$('#bounce').mouseover(function(){
+		$(this).effect('bounce',{times:3}, 300);
+	});
+});
